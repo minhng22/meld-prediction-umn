00d839ffa75a7a0a0610f0cd40ecf4bc4f6c556a
minhnguyen <comrc019@icloud.com>
2024-07-14 21:50:27 -0700

    na

--- pkgs/experiments/diebold_mariano.py
+++ pkgs/experiments/diebold_mariano.py
@@ -1,64 +1,49 @@
 import numpy as np
-from scipy.stats import t
+from scipy.stats import norm
 
 
-def diebold_mariano_util(e1, e2, loss_function='squared'):
+def diebold_mariano_util(e1, e2, loss_fn='two-sided'):
     """
-    Perform the Diebold-Mariano test to compare the predictive accuracy of two forecasting models.
+        Diebold-Mariano test for predictive accuracy.
 
-    Parameters:
-    actual: array-like
-        The actual values.
-    pred1: array-like
-        The predictions from the first model.
-    pred2: array-like
-        The predictions from the second model.
-    loss_function: str
-        The type of loss function to use ('squared' for squared errors, 'absolute' for absolute errors).
-    h: int
-        The forecast horizon.
+        Parameters:
+        e1 (np.array): Forecast errors from model 1.
+        e2 (np.array): Forecast errors from model 2.
+        h (int): Forecast horizon (default is 1).
+        alternative (str): Type of alternative hypothesis, 'two-sided', 'less', or 'greater'.
 
-    Returns:
-    dm_stat: float
-        The Diebold-Mariano test statistic.
-    p_value: float
-        The p-value of the test.
-    """
-
-    # Compute loss differentials
-    if loss_function == 'squared':
-        d = e1**2 - e2**2
-    elif loss_function == 'absolute':
-        d = e1 - e2
-    else:
-        raise ValueError("Unsupported loss function. Use 'squared' or 'absolute'.")
-
-    # Compute mean and variance of loss differentials
+        Returns:
+        DM_stat (float): Diebold-Mariano test statistic.
+        p_value (float): p-value of the test.
+        """
+    d = e1 - e2
     mean_d = np.mean(d)
     var_d = np.var(d, ddof=1)
 
-    # Compute the Diebold-Mariano test statistic
-    dm_stat = mean_d / np.sqrt(var_d / len(d))
+    DM_stat = mean_d / np.sqrt(var_d / len(d))
 
-    # length of forecasting horizon in this case equal length of predicted/actual MELD.
-    h = len(d)
+    if loss_fn == 'two-sided':
+        p_value = 2 * norm.cdf(-abs(DM_stat))
+    elif loss_fn == 'less':
+        p_value = norm.cdf(DM_stat)
+    elif loss_fn == 'greater':
+        p_value = 1 - norm.cdf(DM_stat)
+    else:
+        raise ValueError("alternative must be 'two-sided', 'less', or 'greater'")
 
-    # Compute the p-value
-    p_value = 2 * (1 - t.cdf(abs(dm_stat), df=len(d) - 1))
-
-    return dm_stat, p_value
+    return DM_stat, p_value
 
 
 def diebold_mariano_two_models(e1, e2, model_1_name, model_2_name):
     dm_stat, p_value = diebold_mariano_util(e1, e2)
     alpha = 0.05
     if p_value < alpha:
-        if dm_stat > 0:
-            print(f"Model {model_2_name} is better than Model {model_1_name}. p-value: {p_value}. dm_stat: {dm_stat}")
+        if dm_stat < 0:
+            print(f"Model {model_2_name} is better than {model_1_name}. p-value: {p_value}. dm_stat: {dm_stat}")
         else:
-            print(f"Model {model_1_name} is better than Model {model_2_name}. p-value: {p_value}. dm_stat: {dm_stat}")
+            print(f"Model {model_1_name} is better than {model_2_name}. p-value: {p_value}. dm_stat: {dm_stat}")
     else:
-        print("No significant difference between the two models")
+        print(f"No significant diff between {model_1_name} and {model_2_name}. p-value: {p_value}")
 
 
 # compare Xgboost with the rest of the models.
@@ -67,8 +52,8 @@ def diebold_mariano_test(eval_ress, setup_name):
     baseline_model_name, baseline_model_e = 'xgboost', None
     for eval_res in eval_ress:
         if eval_res[0] == "xgboost":
-            baseline_model_e = eval_res[5]
+            baseline_model_e = eval_res[6]
     for eval_res in eval_ress:
         if eval_res[0] != "xgboost":
-            diebold_mariano_two_models(baseline_model_e, eval_res[5], baseline_model_name, eval_res[0])
+            diebold_mariano_two_models(baseline_model_e, eval_res[6], baseline_model_name, eval_res[0])
 
